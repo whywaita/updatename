9d9d3cdfe3d7ba1ae9539d5a8edb676b8da23851
whywaita <nikoniko.why+github@gmail.com>
2014-05-24 19:19:38 +0900

    twitter系のコピペはこれですね

--- main.rb
+++ main.rb
@@ -3,4 +3,20 @@
 require 'twitter'
 require './key.rb'
 
+# CS/CKを設定しているようだ
+@rest_client = Twitter::REST::Client.new do |config|
+	config.consumer_key	   = Const::CONSUMER_KEY
+	config.consumer_secret     = Const::CONSUMER_SECRET
+	config.access_token        = Const::ACCESS_TOKEN
+	config.access_token_secret = Const::ACCESS_TOKEN_SECRET
+end
+
+# UserStreamはここで接続？
+@stream_client = Twitter::Streaming::Client.new do |config|
+	  config.consumer_key        = Const::CONSUMER_KEY
+	  config.consumer_secret     = Const::CONSUMER_SECRET
+	  config.access_token        = Const::ACCESS_TOKEN
+	  config.access_token_secret = Const::ACCESS_TOKEN_SECRET
+end
+
 
